193a0a0a5d5350938af093e9fd4709fd3337bdf2
Miguel Angel Leon Reyes <angeleonreyes12350@gmail.com>
2025-05-21 23:39:53 -0500

    Second commit

---
+++ src/Principal.java
@@ -0,0 +1,33 @@
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Principal {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+        String direccion = "https://v6.exchangerate-api.com/v6/2e665ce5788c722d7e9cd6f8/latest/USD";
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException |InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        String json = response.body();
+        System.out.println(json);
+    }
+}
